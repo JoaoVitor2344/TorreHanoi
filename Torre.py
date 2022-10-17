e2ad837ed4b959a2c74ead439ba60b9ac3bd8e54
João Vitor <52049527+JoaoVitor2344@users.noreply.github.com>
2022-10-17 20:17:55 -0300

    mover disco alterado

--- Torre.py
+++ Torre.py
@@ -18,10 +18,25 @@ class Torre:
         return len(self._discos)
 
     def last_disco(self):
-        return self._discos[len(self._discos)-1]
+        if self.get_tamanho() == 0:
+            return 0
+        else:
+            return self._discos[len(self._discos)-1]
+
+    def find_disco(self, disco):
+        for i in self._discos:
+            if int(disco) == i:
+                return True
+        
+        return False
 
     def to_string(self):
         print('Quantidade de discos: ' + str(self.get_tamanho()))
 
+        color = 30
+        i = 0
         for disco in self._discos:
-            print(disco)
+            color += i
+            i += 1
+            text = '\033[1;{color};40m' + str(disco) +'\033[0;0m'
+            print(text.format(color=int(color)))
--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 from Torre import Torre
 
-def mover_disco(disco,origem,destino):
+def mover_disco(origem,destino):
     origem = torres[int(origem)-1]
     destino = torres[int(destino)-1]
 
-    if origem.last_disco() == int(disco):
-        origem.desempilha()
-        destino.empilha(disco)
+    if origem.get_tamanho() != 0:
+        disco = int(origem.last_disco())
+
+        if destino.last_disco() > disco or destino.get_tamanho() == 0:
+            origem.desempilha()
+            destino.empilha(disco)
+        else:
+            print('Operação invalida.')
     else:
-        print('Este disco não esta nessa torre')
+        print('Não tem nenhum disco nessa torre.')
+
+def color(color,text): 
+    text = '\033[1;{color};40m' + str(text) +'\033[0;0m'
+    print(text.format(color=int(color)))
 
 if __name__ == '__main__':
-    qntd = int(input('Digite a quantidade de discos: '))
+    qntd = int(input('Digite a quantidade de discos para ser inserido na torre 1: '))
     discos = []
     for i in range(-qntd, 0):
         discos.append(-i)
 
-    pronto = True
-    
     torres = [Torre(discos),Torre([]),Torre([])]
-    while pronto:
-        mover_disco(int(input('Digite o disco: ')), int(input('Digite a origem: ')), int(input('Digite o destino: ')))
-
-        print('')
-
+    while torres[2].get_tamanho() != qntd:
+        color(33, 'Torre 1:')
         torres[0].to_string()
+        color(34, 'Torre 2:')
         torres[1].to_string()
+        color(35, 'Torre 3:')
         torres[2].to_string()
 
-        if torres[2].get_tamanho() == 5:
-            pronto = False
+        print('\nMover disco: ')
+        mover_disco(int(input('Digite a origem: ')), int(input('Digite o destino: ')))
+
+color(32, 'Torre 1:')
+torres[0].to_string()
+color(33, 'Torre 2:')
+torres[1].to_string()
+color(34, 'Torre 3:')
+torres[2].to_string()
 
 print('Parabéns Você terminou a Torre de Hanoi!')        
